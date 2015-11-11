21a30f5446214fd7ec5d8939f0101dc771509582
Mahdi Askari <mahdi-askari@live.com>
2015-11-11 14:21:17 +1100

    hi

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,8 @@
 class AuthorsController < ApplicationController
   def new
+    @page_title= 'Add New Author'
+    @author = Author.new
+    
   end
 
   def create
--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,5 +1,7 @@
 class PublishersController < ApplicationController
   def new
+      @page_title= 'Add New Publisher'
+    @apublisher = Publisher.new
   end
 
   def create
